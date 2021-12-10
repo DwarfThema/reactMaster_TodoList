e83b3d385bd7d4bd54240d8eb27828f2cce589f2
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-10 14:34:17 +0900

    rec fin

--- src/Routes/coin.tsx
+++ src/Routes/coin.tsx
@@ -1,10 +1,12 @@
-import styled from "styled-components"
-import { Route, Routes, useLocation, useMatch, useParams } from "react-router"
-import { useEffect, useState } from "react"
+import styled from "styled-components";
+import { Route, Routes, useLocation, useMatch, useParams } from "react-router";
+import { useEffect, useState } from "react";
 import Chart from "./Chart";
 import Price from "./Price";
 import { Link } from "react-router-dom";
-
+import { useQuery } from "react-query";
+import { fetchInfoData, fetchPrice } from "../api";
+import { Helmet } from "react-helmet";
 
 const Overview = styled.div`
   display: flex;
@@ -32,23 +34,21 @@ const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
   margin: 0 auto;
-    
-`
+`;
 const Header = styled.header`
   height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Title = styled.h1`
-    font-size: 48px;
-    font-weight: 600;
-    color: ${p => p.theme.pointColor};
-`
+  font-size: 48px;
+  font-weight: 600;
+  color: ${(p) => p.theme.pointColor};
+`;
 
-const Loading = styled.div`
-`
+const Loading = styled.div``;
 
 const Tabs = styled.div`
   display: grid;
@@ -57,7 +57,7 @@ const Tabs = styled.div`
   gap: 10px;
 `;
 
-const Tab = styled.span<{activated:boolean}>`
+const Tab = styled.span<{ activated: boolean }>`
   text-align: center;
   text-transform: uppercase;
   font-size: 12px;
@@ -65,138 +65,154 @@ const Tab = styled.span<{activated:boolean}>`
   background-color: rgba(0, 0, 0, 0.5);
   padding: 7px 0px;
   border-radius: 10px;
-  color: ${p=>p.activated?p.theme.pointColor:p.theme.textColor};
-  a{
+  color: ${(p) => (p.activated ? p.theme.pointColor : p.theme.textColor)};
+  a {
     display: block;
   }
 `;
 
-  
-  interface ILocation {
-    state:{
-    name:string;
+interface ILocation {
+  state: {
+    name: string;
+  };
+}
+
+interface RouteParams {
+  coinId: string;
+}
+
+interface IInfoDate {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  contract: string;
+  platform: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
     };
-    }
-
-    
-    interface IInfoDate{
-      id:string;
-      name:string;
-      symbol:string;
-      rank:number;
-      is_new:boolean;
-      is_active:boolean;
-      type:string;
-      contract:string;
-      platform:string;
-      description:string;
-      message:string;
-      open_source:boolean;
-      started_at:string;
-      development_status:string;
-      hardware_wallet:boolean;
-      proof_type:string;
-      org_structure:string;
-      hash_algorithm:string;
-      first_data_at:string;
-      last_data_at:string;
-    }
-
-    interface IPriceData {
-      id: string;
-      name: string;
-      symbol: string;
-      rank: number;
-      circulating_supply: number;
-      total_supply: number;
-      max_supply: number;
-      beta_value: number;
-      first_data_at: string;
-      last_updated: string;
-      quotes: {
-        USD: {
-          ath_date: string;
-          ath_price: number;
-          market_cap: number;
-          market_cap_change_24h: number;
-          percent_change_1h: number;
-          percent_change_1y: number;
-          percent_change_6h: number;
-          percent_change_7d: number;
-          percent_change_12h: number;
-          percent_change_15m: number;
-          percent_change_24h: number;
-          percent_change_30d: number;
-          percent_change_30m: number;
-          percent_from_price_ath: number;
-          price: number;
-          volume_24h: number;
-          volume_24h_change_24h: number;
-        };
-      };
-    }
+  };
+}
 
 function Coin() {
-    const { coinId } = useParams()
-    const [loadingNow, loadingNext] = useState(true)
+  const { coinId } = useParams() as RouteParams;
 
-    const [priceNow, priceNext] = useState<IPriceData>()
-    const [infoNow, infoNext] =useState<IInfoDate>()
+  const { state } = useLocation() as ILocation;
 
-    const {state} = useLocation() as ILocation;
+  const chartMatch = useMatch("/:coinId/chart");
+  const priceMatch = useMatch("/:coinId/price");
 
-    const chartMatch = useMatch("/:coinId/chart");
-    const priceMatch = useMatch("/:coinId/price");
-    
+  const { isLoading: infoLoading, data: infoData } = useQuery<IInfoDate>(
+    ["info", coinId],
+    () => fetchInfoData(coinId)
+  );
 
-    useEffect(()=>{
-      (async()=>{
-        const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-        const priceDate = await(await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
-        infoNext(infoData);        
-        priceNext(priceDate);
-        loadingNext(false)
-      })()
+  const { isLoading: priceLoading, data: priceata } = useQuery<IPriceData>(
+    ["price", coinId],
+    () => fetchPrice(coinId),
+    {
+      refetchInterval: 5000,
+    }
+  );
 
-    },[coinId])
+  const loadingNow = infoLoading || priceLoading;
 
-    return <Container>
-        <Header>
+  return (
+    <Container>
+      <Helmet>
+        <title>
+          {state?.name
+            ? state.name
+            : loadingNow
+            ? "Loading..."
+            : infoData?.name}
+        </title>
+      </Helmet>
+      <Header>
         <Title>
-          {state?.name ? state.name : loadingNow ? "Loading..." : infoNow?.name}
+          {state?.name
+            ? state.name
+            : loadingNow
+            ? "Loading..."
+            : infoData?.name}
         </Title>
-        </Header>      {loadingNow ? (
+      </Header>{" "}
+      {loadingNow ? (
         <Loading>Loading...</Loading>
       ) : (
         <>
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{infoNow?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${infoNow?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoNow?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>${priceata?.quotes.USD.price.toFixed(3)}</span>
             </OverviewItem>
           </Overview>
-          <Description>{infoNow?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceNow?.total_supply}</span>
+              <span>{priceata?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceNow?.max_supply}</span>
+              <span>{priceata?.max_supply}</span>
             </OverviewItem>
           </Overview>
 
           <Tabs>
-            <Tab activated={chartMatch !== null} >
+            <Tab activated={chartMatch !== null}>
               <Link to={`/${coinId}/chart`}>Chart</Link>
             </Tab>
             <Tab activated={priceMatch !== null}>
@@ -205,12 +221,13 @@ function Coin() {
           </Tabs>
 
           <Routes>
-            <Route path="chart" element={<Chart/>}/>
-            <Route path="price" element={<Price/>}/>
+            <Route path="chart" element={<Chart coinId={coinId} />} />
+            <Route path="price" element={<Price />} />
           </Routes>
         </>
       )}
-      </Container>
+    </Container>
+  );
 }
 
-export default Coin
+export default Coin;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
-import App from './App';
-import { theme } from './theme';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={true} />
+  </QueryClientProvider>,
+  document.getElementById("root")
 );
